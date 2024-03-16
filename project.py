916039f64b90b35c44a6b8267ecf2ad355b5b15c
Mateusz Marczak <marczak812@gmail.com>
2024-03-16 16:53:00 +0100

    Add function to display shopping list

--- project.py
+++ project.py
@@ -2,3 +2,13 @@ shopping_list = {
     "piekarnia" : ["chleb", "pączek", "bułki"],
     "warzywniak" : ["marchew", "seler", "rukola"]
 } 
+
+i = 0
+
+print("Lista zakupów")
+for (keys, values) in shopping_list.items():
+    for value in values:
+        values = values + [value.capitalize()]
+        values.remove(value)
+        i = i + 1
+    print(f"Idę do {keys.capitalize()}, kupuję tu następujące rzeczy: {values}")
