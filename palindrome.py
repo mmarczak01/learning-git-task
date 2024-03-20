1c0c22dccd10e049b09369481f9998f7d9327bca
Mateusz Marczak <marczak812@gmail.com>
2024-03-20 23:15:20 +0100

    Modify function to accept case sensitive differences and to verify characters

--- palindrome.py
+++ palindrome.py
@@ -1,14 +1,14 @@
-def is_palindrome(word):
+def is_palindrome(word: str):
     """
     Prints true or false depends on 
     if the given word argument is a palindrome or not
     """
-    if word == word[:: -1]:
-        return True
+    if word.isalnum():
+        return word.lower() == word.lower() [:: -1] 
     else:
         return False
-    
-print(is_palindrome("potop"))
+       
+print(is_palindrome("Potop"))
 print(is_palindrome("nos"))
-print(is_palindrome("kajak"))
+print(is_palindrome("kaJaK"))
 print(is_palindrome("brat"))
